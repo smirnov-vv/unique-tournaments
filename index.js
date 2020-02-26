47a6df2cb06f5f3e1e5ceba49a38bcc8ea4f111f
Viacheslav Smirnov <smirnov-83@list.ru>
2020-02-26 18:47:19 +0300

    first upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const url = 'https://rttf.ru/tournaments/21155';
+let response = await fetch(url);
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  // получаем тело ответа (см. про этот метод ниже)
+  let json = await response.json();
+} else {
+  alert("Ошибка HTTP: " + response.status);
